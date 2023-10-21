970e264409992e33a9ecd83a94d6791a3c60e701
hamza herbou <hamza95herbou@gmail.com>
2023-10-21 12:20:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ const Letters = [
     {letter: 'ف', alts: ['ف', 'ڡ', '؋', 'ڡ']},
     {letter: 'ق', alts: ['ڡ', 'ق']},
     {letter: 'ي', alts: ['ي', 'ې', 'ى']},
+    {letter: 'ئ', alts: ['ئ', 'ى']},
     {letter: 'ك', alts: ['ك', 'گ', 'ګ']}
 ]
 
@@ -34,25 +35,94 @@ const text_UI   = document.querySelector(".text")
 const result_UI = document.querySelector(".result")
 const btn_UI    = document.querySelector(".btn")
 
+const rangeExtendLength_UI    = document.querySelector(".inp-extend-length")
+const checkboxExtendRandom_UI = document.querySelector(".inp-extend-rand")
+
+
 
 btn_UI.addEventListener("click", e => {
-    const text = text_UI.value
-    let result = ""
+    let text = text_UI.value
 
-    text.split("").forEach(letter=>{
-        let alt = Letters.find(l => l.letter == letter)
+    let textExtended = ""
+    text.split(' ').forEach(word => textExtended+=extendWord(word)+' ')
 
-        if (alt)
-            result += getRandom(alt.alts)
-        else
-            result += letter
-    })
+    text = textExtended
+    text = transformLetters(text)
 
-    result_UI.value = result
+    result_UI.value = text
 })
 
 
-function getRandom(arr) {
-    return arr[Math.floor(Math.random()*arr.length)]
+function extendWord(word){
+    let result = ""
+    const _ = 'ـ'
+         
+    const nonExtendedLetters = ['و', 'ز', 'ر', 'ذ', 'د', 'أ', 'إ','آ', 'ا', ' ', 'ؤ','ء', 'ئ', 'ى', 'ة']
+
+    for (let i = 0; i < word.length-1; i++) {
+        const letter = word[i]
+        result += letter
+
+        const isNonExtendedLetter = nonExtendedLetters.find(l=>l==letter)
+        if (!isNonExtendedLetter) {
+            len = rangeExtendLength_UI.value || 0
+
+            if (checkboxExtendRandom_UI.checked)
+                len = rand(0, len)
+
+            for (let j = 0; j < len; j++)
+                result += _
+        }
+        
+    }
+
+    result += word[word.length-1]
+
+    return result
 }
 
+
+function transformLetters(text){
+    let result = ""
+    text.split("").forEach(letter=>{
+        let alt = Letters.find(a => a.letter == letter)
+
+        if (alt)
+            result += alt.alts.getRandomItem()
+        else
+            result += letter
+    })
+    return result
+}
+
+
+
+
+
+
+
+// Load Settings
+window.onload = e => {
+    const extendLength = parseInt(localStorage.getItem("decorabic-settings-extendLength")) || 0
+    const extendRandom = localStorage.getItem("decorabic-settings-isExtendRand") === "true"
+    rangeExtendLength_UI.value = extendLength
+    checkboxExtendRandom_UI.checked = extendRandom
+}
+
+// Save settings
+rangeExtendLength_UI.addEventListener("input",e=>localStorage.setItem("decorabic-settings-extendLength", rangeExtendLength_UI.value))
+checkboxExtendRandom_UI.addEventListener("input",e=>localStorage.setItem("decorabic-settings-isExtendRand", checkboxExtendRandom_UI.checked))
+
+
+
+
+
+
+function rand(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+
+Array.prototype.getRandomItem = function() {
+    return this[Math.floor(Math.random()*this.length)]
+}
